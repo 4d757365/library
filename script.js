3dc052f95bc9e253f41c4c9d58ae9a5edee3e8bd
Muse Tesfaye <tesfaye.musee@gmail.com>
2022-07-23 14:34:06 -0400

    Completed script for the library

--- script.js
+++ script.js
@@ -2,25 +2,39 @@ const $title = document.querySelector("#title");
 const $author = document.querySelector("#author");
 const $pages = document.querySelector("#pages");
 const $read = document.querySelector("#read");
+const $library = document.querySelector("#library");
+
 const $form = document.querySelector("form").addEventListener("submit", (e)=>{
     e.preventDefault();
     addBookToLibrary();
+    updateDisplay();
     clear();
 });
+
+const $table = document.querySelector("table").addEventListener("click", (e) => {
+    const currentTarget = e.target.parentNode.parentNode.childNodes[1];
+    if (e.target.innerHTML == "delete") {
+        deleteBook(find(myLibrary, currentTarget.innerText));
+    }
+    if (e.target.classList.contains("status")) {
+      change(find(myLibrary, currentTarget.innerText));
+    }
+    updateDisplay();
+  });
 
 let myLibrary = [
     {
         title: "Game of Thrones",
         author: "George R. R. Martin",
         pages: 694,
-        read: false
+        read: 'read'
     },
 
     {
         title: "Dune",
         author: "Frank Herbert",
         pages: 412,
-        read: false
+        read: 'not read'
     }
 ];
 
@@ -52,4 +66,53 @@ function clear() {
     $pages.value = '';
     $read.selectedIndex = 0;
 }
+
+function updateDisplay() {
+    $library.innerHTML = "";
+    myLibrary.forEach((book) => {
+      const b = `
+        <tr>
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td>${book.pages}</td>
+          <td><button class="status">${book.read}</button></td>
+          <td><button class="delete">delete</button></td>
+        </tr>
+        `;
+      $library.insertAdjacentHTML("afterbegin", b);
+    });
+}
+
+function find(library, title) {
+    if(library.length === 0 || library === null) 
+    {
+        return;
+    }
+    else 
+    {
+        for (book of library)
+        {
+            if(book.title === title)
+            {
+                return library.indexOf(book);
+            }
+        }
+    }
+}
+
+function deleteBook(book) {
+    myLibrary.splice(book, book + 1);
+}
+
+function change(Book) {
+    if(myLibrary[Book].read === "read") 
+    {
+        myLibrary[Book].read = "not read";
+    }
+    else 
+    {
+        myLibrary[Book].read = "read";
+    }
+}
+
 
